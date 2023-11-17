8d96cfe1fb5241c71302e459f84e220f583b570b
lucasaffonso0 <lucasafonso.ifal@gmail.com>
2023-11-17 17:53:07 -0300

    feat: changing config for production

--- config.py
+++ config.py
@@ -20,9 +20,20 @@ class MockConfig:
 
 
 class ProdConfig():
+
+    MONGODB_USER = os.getenv('MONGODB_USER')
+    MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+    MONGODB_HOST = os.getenv('MONGODB_HOST')
+    MONGODB_DB = os.getenv('MONGODB_DB')
+    REPLICASET = 'replicaSet=atlas-qkejxn-shard-0'
+    AUTH = 'authSource=admin&retryWrites=true&w=majority'
     MONGODB_SETTINGS = {
-        'db': os.getenv('MONGO_DB'),
-        'host': os.getenv('MONGO_HOST'),
-        'username': os.getenv('MONGO_USER'),
-        'password': os.getenv('MONGO_PASSWORD')
+        'host': 'mongodb://%s:%s@%s:27017/%s?ssl=true&%s&%s' % (
+            MONGODB_USER,
+            MONGODB_PASSWORD,
+            MONGODB_HOST,
+            MONGODB_DB,
+            REPLICASET,
+            AUTH
+            )
     }
--- tests/test_application.py
+++ tests/test_application.py
@@ -18,7 +18,7 @@ class TestApplication():
                 "email": "teste@gmail.com",
                 "birth_date": "2000-05-10"
             }
-    
+
     @pytest.fixture
     def invalid_user(self):
         return {
@@ -32,16 +32,16 @@ class TestApplication():
     def test_get_users(self, client):
         response = client.get('/users')
         assert response.status_code == 200
-    
+
     def test_post_users(self, client, valid_user, invalid_user):
         response = client.post('/user', json=valid_user)
         assert response.status_code == 200
         assert b'successfully' in response.data
-        
+
         response = client.post('/user', json=invalid_user)
         assert response.status_code == 400
         assert b'invalid' in response.data
-    
+
     def test_get_user(self, client, valid_user, invalid_user):
         response = client.get(f'/user/{valid_user["cpf"]}')
         assert response.status_code == 200
@@ -49,8 +49,9 @@ class TestApplication():
         assert response.json[0]['last_name'] == 'Lucas'
         assert response.json[0]['cpf'] == '887.001.790-78'
         assert response.json[0]['email'] == 'teste@gmail.com'
-        assert response.json[0]['birth_date']['$date'] == '2000-05-10T00:00:00Z'
-        
+        birth_date = response.json[0]['birth_date']['$date']
+        assert birth_date == '2000-05-10T00:00:00Z'
+
         response = client.get(f'/user/{invalid_user["cpf"]}')
         assert response.status_code == 404
         assert b'User does not' in response.data
