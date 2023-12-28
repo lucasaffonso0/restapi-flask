63d992af42fa8376a6c432664d1db8a1c280edbf
lucasaffonso0 <lucasafonso.ifal@gmail.com>
2023-12-28 15:21:51 -0300

    feat: add patch endpoint and testing

--- application/app.py
+++ application/app.py
@@ -59,3 +59,16 @@ class User(Resource):
             return jsonify(response)
 
         return {'message': 'User does not exist in database'}, 404
+
+    def patch(self):
+        data = _user_parser.parse_args()
+
+        if not CPF().validate(data['cpf']):
+            return {'message': 'CPF is invalid!'}, 400
+
+        response = UserModel.objects(cpf=data['cpf'])
+        if response:
+            response.update(**data)
+            return {'message': 'User updated successfully'}, 200
+        else:
+            return {'message': 'User does not exist in database'}, 404
--- tests/test_application.py
+++ tests/test_application.py
@@ -55,3 +55,14 @@ class TestApplication():
         response = client.get(f'/user/{invalid_user["cpf"]}')
         assert response.status_code == 404
         assert b'User does not' in response.data
+
+    def test_patch_users(self, client, valid_user):
+        valid_user["first_name"] = "Joseé"
+        response = client.patch('/user', json=valid_user)
+        assert response.status_code == 200
+        assert b'updated' in response.data
+
+        valid_user["cpf"] = "878.197.570-86"
+        response = client.patch('/user', json=valid_user)
+        assert response.status_code == 404
+        assert b'does not exist in database' in response.data
