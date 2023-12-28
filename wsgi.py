5003f9d78ecd8a502dccdc5fc0d365e22f8cefb6
lucasaffonso0 <lucasafonso.ifal@gmail.com>
2023-12-28 16:40:55 -0300

    feat: bandit for SAST

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,13 @@
 from application import create_app
 import os
-
+import socket
 
 if os.getenv("FLASK_ENV") == "development":
     app = create_app("config.DevConfig")
 else:
     app = create_app("config.ProdConfig")
 
+ip_address = socket.gethostbyname(socket.gethostname())
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=os.getenv("PORT", 5000), debug=True)
+    app.run(host=ip_address, port=os.getenv("PORT", 5000), debug=True)
