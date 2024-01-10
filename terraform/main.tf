507d9cf434ba28a1b796533ada871a478dc69c53
lucasaffonso0 <lucasafonso.ifal@gmail.com>
2024-01-10 16:54:49 -0300

    feat: update source terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "kubernetes" {
-  source       = "/home/lucas/terraform-eks"
+  source       = "git@github.com:lucasaffonso0/restapi-flask.git?ref=flask-api"
   cidr_block   = "10.0.0.0/16"
   project_name = "restapi"
   region       = "us-west-2"
